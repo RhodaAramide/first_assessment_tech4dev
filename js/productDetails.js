b47c512d45b139e4e795b81d1f3e860a7232120a
oreoluwa212 <olabisiabolarin5@gmail.com>
2024-11-29 13:39:42 +0100

    Added some styling

--- js/productDetails.js
+++ js/productDetails.js
@@ -4,19 +4,19 @@ const displayProductDetails = (product) => {
   const productDetailsDiv = document.getElementById("product-details");
 
   const productImage = document.createElement("img");
-  console.log(product?.image);
-  productImage.src = product?.image;
-  productImage.alt = product?.title;
+
+  productImage.src = product.image;
+  productImage.alt = product.title;
   productImage.className = "product-image";
 
   const productTitle = document.createElement("h1");
   productTitle.textContent = product?.title;
 
   const productPrice = document.createElement("p");
-  productPrice.textContent = `Price: $${product?.price}`;
+  productPrice.textContent = `Price: $${product.price}`;
 
   const productDescription = document.createElement("p");
-  productDescription.textContent = product?.description;
+  productDescription.textContent = product.description;
 
   productDetailsDiv.appendChild(productImage);
   productDetailsDiv.appendChild(productTitle);
@@ -30,5 +30,6 @@ const getProductIdFromQuery = () => {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get("productId");
 };
+
 const productId = getProductIdFromQuery();
 productService.getProductById(productId).then(displayProductDetails);
