73abe3c1056b9bfae5108dba2604ce61fb97e89c
oreoluwa212 <olabisiabolarin5@gmail.com>
2024-12-03 10:59:10 +0100

    Added the other sections of the page

--- src/js/elementManipulation.js
+++ src/js/elementManipulation.js
@@ -4,7 +4,7 @@ const createEventCard = (product) => {
   // Create the event card div
   const eventCard = document.createElement("div");
   eventCard.className =
-    "bg-white border border-gray-200 rounded-lg overflow-hidden shadow-md rounded-[10px] flex flex-col justify-between";
+    "bg-white border border-gray rounded-lg overflow-hidden shadow-md rounded-[10px] flex flex-col justify-between";
 
   // Create the image element
   const eventImage = document.createElement("img");
@@ -15,30 +15,27 @@ const createEventCard = (product) => {
 
   // Create the event details div
   const eventDetails = document.createElement("div");
-  eventDetails.className = "p-6 flex flex-col flex-grow";
+  eventDetails.className = "px-4 py-6 flex flex-col flex-grow";
 
   // Create the event title
   const eventTitle = document.createElement("h3");
   eventTitle.textContent = product.title;
-  eventTitle.className =
-    "text-[16px] leading-[18.75px] font-semibold text-black mb-2";
+  eventTitle.className = "text-base font-semibold text-black mb-2";
 
   // Create the event price
   const eventPrice = document.createElement("p");
   eventPrice.textContent = `$${product.price}`;
-  eventPrice.className =
-    "mb-4 text-black text-[14px] leading-[16.41px] font-medium";
+  eventPrice.className = "mb-4 text-black text-sm font-medium";
 
   // Create the event description
   const eventDescription = document.createElement("p");
   eventDescription.textContent = product.description;
-  eventDescription.className = "mb-4 text-black text-[14px] leading-[16.41px]";
+  eventDescription.className = "mb-4 text-black text-sm";
 
   // Create the view details link
   const viewDetailsLink = document.createElement("a");
   viewDetailsLink.href = "/index.html?productId=" + product.id;
-  viewDetailsLink.className =
-    "text-[#432361] text-[14px] leading-[16.41px] mt-auto";
+  viewDetailsLink.className = "text-primary text-sm font-semibold mt-auto";
   viewDetailsLink.textContent = "View details";
   viewDetailsLink.dataset.productId = product.id;
   // Add an event listener to the view details link
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ["14px", "16.41px"],
+        base: ["16px", "18.75px"],
+        lg: ["32px", "37.5px"],
+      },
+      colors: {
+        primary: "#432361",
+        secondary: "#783EAD",
+        white: "#FFFFFF",
+        black: "#000000",
+        gray: "#E0E0E0",
+        dark: "#4F4F4F",
+      },
+      fontFamily: {
+        gilroy: ["gilroy", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
